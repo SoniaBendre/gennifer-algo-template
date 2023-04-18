b6261333ba976dbdf907d66ace69d8b37ae08643
Sonia Bendre <65863659+SoniaBendre@users.noreply.github.com>
2023-04-18 00:38:31 -0400

    Update gennifer_api.py

--- gennifer_api.py
+++ gennifer_api.py
@@ -1,23 +1,20 @@
 import os
 import pandas as pd
 
-from arboreto.algo import grnboost2, genie3
-from arboreto.utils import load_tf_names
+import ppcor
 from distributed import Client
 
 DATASET_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sample_data')
 
 def generateInputs(dataset_uri):
-    return pd.read_csv(os.path.join(DATASET_PATH, dataset_uri), header=0, index_col=0)
+    inputExpr = pd.read_csv(os.path.join(DATASET_PATH, dataset_uri), header=0, index_col=0)
+    geneNames = inputExpr.index.values
+    inputExpr.index = geneNames
+    return inputExpre
 
-def run(dataset, algo):
-    client = Client(processes = False)    
 
-    if algo == 'GENIE3':
-        network = genie3(dataset.to_numpy(), client_or_address = client, gene_names = dataset.columns)
-
-    elif algo == 'GRNBoost2':
-        network = grnboost2(dataset.to_numpy(), client_or_address = client, gene_names = dataset.columns)
+def run(inputExpre):
+    network = pcor(x = np.transpose(inputExpr.values), method = "spearman")
     return network
 
 def parseOutput(network):
